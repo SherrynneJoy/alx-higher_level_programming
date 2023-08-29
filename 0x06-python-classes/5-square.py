28027cb781e598849f9d67517f07eb271c6a58f0
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-08-29 11:31:47 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Creating a square object"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    """Getter and setter method to retrieve and update the object"""
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """Creating public instance methods"""
+    def area(self):
+        return (self.__size * self.__size)
+    def my_print(self):
+        """print the square with the # character"""
+        for i in range(0, self.__size):
+            [print("#", end="")for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
