1faf4fabe56c4b0722df65e20e561140e394a60c
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-05 10:18:00 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/tests/2-matrix_divided.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def matrix_divided(matrix, div):
-    """Divides two matrices which are lists of lists.
-    If either matrix is not a list of lists, a TypeError is raised.
-    div must be an int or float. Otherwise, a TypeError is raised.
-    div should be greater than zero. Otherwise a ZeroDivisionError
-    is raised."""
-
