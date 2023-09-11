f16acca84bf339889d42d66dc363a11a7e268f40
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-11 08:52:29 -0700

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that returns the list of available attributes and methods"""
+
+
+def lookup(obj):
+    """Returns a list object"""
+    list = [dir(obj)]
+    return (list)
