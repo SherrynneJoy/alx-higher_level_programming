d99daa2fc470bdba7916857237a1889376064570
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-11 19:48:27 -0700

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """The square is made up of sides called (size)"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    """__str__ prints our description"""
+    def __str__(self):
+        """returns the printed string"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return (string)
