a02ff0a57fde4b506a362b16dbf4c6fd590a88f8
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-12 09:19:16 -0700

    Load,add,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,16 +8,15 @@ import os
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file')
+    .load_from_json_file
 
     if os.path.exists("add_item.json"):
         elements = load_from_json_file("add_item.json")
     else:
         elements = []
 
-    #adding command line args to the list
     for arg in sys.argv[1:]:
         elements.append(arg)
 
-    #save the list in a file called add_item.json
     save_to_json_file(elements, "add_item.json")
