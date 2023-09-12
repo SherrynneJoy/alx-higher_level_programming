952ddd281e495b2029fc052b91367784cb5d8c42
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-11 19:54:48 -0700

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """a class Square that inherits from Rectangle (9-rectangle.py)"""
+
 Rectangle = __import__('9-rectangle').Rectangle
+
+"""Creating a subclass that inherits from rectangle"""
 
 
 class Square(Rectangle):
