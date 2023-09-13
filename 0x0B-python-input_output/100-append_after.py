2049ecb396623d7fa8cc17a7cd7e56a2e7bacb99
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-13 10:21:02 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that inserts a line of text to a file, after each line
+containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file, after each line containing
+    a specific string"""
+    with open(filename, "r+", encoding="utf-8") as f:
+        lines = f.readlines()
+        f.seek(0)
+
+        """iterating over the file"""
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string + '\n')
