dbc3d9a96bff5ec944f0d58fd46c8aceb9663aa7
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-09-18 19:58:02 -0700

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Creates the base class for all other classes"""
+import json
 
 
 class Base:
@@ -13,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    """JSON library functions"""
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """that returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """test the functionality of the code"""
 import unittest
+import json
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -21,6 +23,14 @@ class TestBase(unittest.TestCase):
         self.assertEqual(12, Base(12).id)
         b5 = Base()
         self.assertEqual(b5.id, 4)
+
+"""class to test json string method"""
+class test_json_string(unittest.TestCase):
+    """tests the string method of json"""
+    def test_str_json(self):
+        """tests for correct output"""
+        r1 = Rectangle(10, 7, 2, 8)
+        self.assertEqual(str, type(Base.to_json_string([dictionary])))
 
 if __name__ == '__main__':
     unittest.main()
