b55687c3701b39e622933eca031fda172e59eac0
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-10-18 08:43:12 -0700

    Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- listing all cities in hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM hbtn_0d_usa.cities
+JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
