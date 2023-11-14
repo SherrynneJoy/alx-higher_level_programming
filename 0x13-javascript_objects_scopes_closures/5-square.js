b82741b1fbed148380f96f49fe8043d8e24cd46a
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-14 05:18:07 -0800

    Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // an empty object
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    if (this.width !== undefined && this.height !== undefined) {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  }
+
+  double () {
+    if (this.width !== undefined && this.height !== undefined) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
