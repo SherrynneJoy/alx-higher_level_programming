40be6a8dc13eca53a74b43cf51e2fe43cf7c8a84
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-15 00:38:48 -0800

    Add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const a = parseInt(args[0]);
-const b = parseInt(args[1]);
 
-function add (a, b) {
+exports.add = function add (a, b) {
   return (a + b);
 };
-const sum = add(a, b);
-module.exports = sum;
