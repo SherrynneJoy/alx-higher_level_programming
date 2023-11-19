4cce12815912befcb345be57437578132b72ae08
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-19 09:48:03 -0800

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module connects to a db, creates a curor, exocutes and prints output"""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                           passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     curs = conn.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id ASC")
     total_rows = curs.fetchall()
