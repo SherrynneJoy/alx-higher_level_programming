6aeea1c212e5258decd2893fec63f0c531aa0f72
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-11-30 05:39:17 -0800

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#a Bash script that takes in a URL, sends a request to that URL,
+#and displays the size of the body of the response
+
+url="$1"
+answer=$(curl -s "$url" | tr -d '[:space:]' | wc -c)
