686c8016259177be95746db1bd5eac75fef78af0
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-12-01 08:15:52 -0800

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+"""a Python script that takes in a URL, sends a request to the"""
+"""URL and displays the body of the response."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+
+        print(r.text)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {e.response.stats_code]")
