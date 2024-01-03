4caa194f06313baf8c0745a354c8d054e249208a
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-01-03 07:40:25 -0800

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-
-const axios = require('axios');
-
-function checkStatus (url) {
-  axios.get(url)
-    .then(response => {
-      console.log(`code: ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`code: ${error.message}`);
-      process.exit(1); // Terminate the script with an error code
-    });
-}
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node script.js <URL>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
-checkStatus(url);
+const request = require('request');
+request.get(process.argv[2].on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
