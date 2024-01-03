3b4149d74d62b740e799c1da223f4c56e9046456
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-01-03 07:24:19 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const writeFile = (fp, info) => {
-  try {
-    fs.writeFileSync(fp, info, 'utf-8');
-    console.log(`info was written successfully into: ${fp}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-};
-
-if (process.argv.length !== 4) {
-  console.log('Usage: node write_file.js <file_path> <info_to_write>');
-} else {
-  const fp = process.argv[2];
-  const infoToWrite = process.argv[3];
-  writeFile(fp, infoToWrite);
-}
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
